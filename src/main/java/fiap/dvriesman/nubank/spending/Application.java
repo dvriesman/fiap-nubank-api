cd571c2f0b82c34ed21019d18df6c7e3d522bd29
Denny Vriesman <dvriesman@gmail.com>
2020-03-12 20:28:00 -0300

    Ajustes finais

--- src/main/java/fiap/dvriesman/nubank/spending/Application.java
+++ src/main/java/fiap/dvriesman/nubank/spending/Application.java
@@ -27,11 +27,13 @@ public class Application {
 	@Bean
 	InitializingBean sendDatabase() {
 		return () -> {
-			repository.save(new Spending("denny", Util.getDateFromString("01/12/2010 10:00"), new BigDecimal(1000.00), "combustivel" ));
-			repository.save(new Spending("denny", Util.getDateFromString("15/01/2011 10:00"), new BigDecimal(1000.00), "lazer" ));
-			repository.save(new Spending("denny", Util.getDateFromString("14/12/2011 10:00"), new BigDecimal(1000.00), "saude" ));
-			repository.save(new Spending("denny", Util.getDateFromString("13/12/2012 10:00"), new BigDecimal(1000.00), "lazer" ));
-			repository.save(new Spending("joao", Util.getDateFromString("15/07/2001 10:00"), new BigDecimal(1000.00), "alimentacao" ));
+			repository.save(new Spending("denny", Util.getDateFromString("02/03/2020 10:00"), new BigDecimal(513.10), "alimentacao" ));
+			repository.save(new Spending("denny", Util.getDateFromString("01/01/2020 10:00"), new BigDecimal(321.00), "combustivel" ));
+			repository.save(new Spending("denny", Util.getDateFromString("01/12/2010 10:00"), new BigDecimal(1030.00), "combustivel" ));
+			repository.save(new Spending("denny", Util.getDateFromString("15/01/2011 10:00"), new BigDecimal(200.00), "lazer" ));
+			repository.save(new Spending("denny", Util.getDateFromString("14/12/2011 10:00"), new BigDecimal(4230.00), "saude" ));
+			repository.save(new Spending("denny", Util.getDateFromString("13/12/2012 10:00"), new BigDecimal(20.00), "lazer" ));
+			repository.save(new Spending("joao", Util.getDateFromString("15/07/2001 10:00"), new BigDecimal(650.00), "alimentacao" ));
 		};
 	}
 
